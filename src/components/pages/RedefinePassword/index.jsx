5a229f8351e2621fd3befd677ad58bb851a72cf4
Marcos Oliveira <marcosgarcia.oliveira02@gmail.com>
2022-07-05 20:45:06 -0300

    feat: add reset password system

--- src/components/pages/RedefinePassword/index.jsx
+++ src/components/pages/RedefinePassword/index.jsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import * as React from 'react'
+import { useForm } from 'react-hook-form'
+import { useLocation } from 'react-router-dom'
 
+import { ErrorMessage } from '@hookform/error-message'
+import { yupResolver } from '@hookform/resolvers/yup'
 import {
 	IonBackButton,
 	IonButtons,
@@ -12,16 +16,78 @@ import {
 	IonPage,
 	IonText,
 	IonTitle,
-	IonToolbar
+	IonToolbar,
+	useIonLoading,
+	useIonRouter,
+	useIonToast
 } from '@ionic/react'
 import { eyeOffOutline, eyeOutline } from 'ionicons/icons'
+import * as Yup from 'yup'
 
+import { supabase } from '../../../utils/supabaseClient'
 import Button from '../../ui/Button'
 
 const RedefinePassword = () => {
 	const [showNewPassword, setShowNewPassword] = React.useState(false)
 	const [showRepeatNewPassword, setShowRepeatNewPassword] =
 		React.useState(false)
+
+	const router = useIonRouter()
+	const [showLoading, hideLoading] = useIonLoading()
+	const [showToast] = useIonToast()
+
+	const getAcessToken = JSON.parse(
+		window.localStorage.getItem('supabase.auth.token')
+	)?.currentSession?.access_token
+
+	const schema = Yup.object().shape({
+		password: Yup.string()
+			.min(6, 'A senha deve ter no mínimo 6 caracteres')
+			.required('A senha é obrigatória'),
+		confirmPassword: Yup.string()
+			.required('Confirmação da senha é obrigatória')
+			.oneOf([Yup.ref('password')], 'As senhas não conferem')
+	})
+
+	const {
+		handleSubmit,
+		register,
+		formState: { errors }
+	} = useForm({
+		mode: 'onChange',
+		resolver: yupResolver(schema)
+	})
+
+	const handleNewPassword = async data => {
+		await showLoading()
+		try {
+			const { data: updateUserData, error } =
+				await supabase.auth.api.updateUser(getAcessToken, {
+					password: data.password
+				})
+			if (updateUserData) {
+				await showToast({
+					message: 'Senha alterada com sucesso',
+					duration: 2000
+				})
+				router.push('/login')
+			}
+
+			if (error) {
+				await showToast({
+					message: error.message,
+					duration: 2000
+				})
+			}
+		} catch (e) {
+			await showToast({
+				message: e.error_description || e.message,
+				duration: 2000
+			})
+		} finally {
+			await hideLoading()
+		}
+	}
 
 	return (
 		<IonPage>
@@ -40,57 +106,76 @@ const RedefinePassword = () => {
 						conta e enviaremos um link com instruções.
 					</IonLabel>
 				</div>
-
-				<IonItem className="mt-16" lines="none">
-					<IonLabel position="stacked">Digite sua nova senha</IonLabel>
-					<IonInput
-						placeholder="*********"
-						type={showNewPassword ? 'text' : 'password'}
-					/>
-					<Button
-						className="absolute top-[35px] right-2 w-max z-[3] text-2xl bg-transparent"
-						type="button"
-						onClick={() => {
-							showNewPassword
-								? setShowNewPassword(false)
-								: setShowNewPassword(true)
-						}}
-					>
-						<IonIcon
-							icon={showNewPassword ? eyeOffOutline : eyeOutline}
-							className="text-[#AC8FBF]"
+				<form onSubmit={handleSubmit(handleNewPassword)}>
+					<IonItem className="mt-16" lines="none">
+						<IonLabel position="stacked">Digite sua nova senha</IonLabel>
+						<IonInput
+							id="password"
+							name="password"
+							placeholder="*********"
+							type={showNewPassword ? 'text' : 'password'}
+							{...register('password')}
 						/>
-					</Button>
-				</IonItem>
-
-				<IonItem lines="none">
-					<IonLabel position="stacked">Repita sua nova senha</IonLabel>
-					<IonInput
-						placeholder="*********"
-						type={showRepeatNewPassword ? 'text' : 'password'}
-					/>
-					<Button
-						className="absolute top-[35px] right-2 w-max z-[3] text-2xl bg-transparent"
-						type="button"
-						onClick={() => {
-							showRepeatNewPassword
-								? setShowRepeatNewPassword(false)
-								: setShowRepeatNewPassword(true)
-						}}
-					>
-						<IonIcon
-							icon={showRepeatNewPassword ? eyeOffOutline : eyeOutline}
-							className="text-[#AC8FBF]"
+						<ErrorMessage
+							errors={errors}
+							name="password"
+							as={<div style={{ color: 'red' }} />}
 						/>
-					</Button>
-				</IonItem>
+						<Button
+							className="absolute top-[35px] right-2 w-max z-[3] text-2xl bg-transparent"
+							type="button"
+							onClick={() => {
+								showNewPassword
+									? setShowNewPassword(false)
+									: setShowNewPassword(true)
+							}}
+						>
+							<IonIcon
+								icon={showNewPassword ? eyeOffOutline : eyeOutline}
+								className="text-[#AC8FBF]"
+							/>
+						</Button>
+					</IonItem>
 
-				<Button className="bg-purple-100 py-5 my-6 mt-16">
-					<IonText className="text-white text-lg">Redefinir</IonText>
-				</Button>
-				<Button className="bg-danger py-5 my-6">
-					<IonText className="text-white text-lg">Cancelar</IonText>
-				</Button>
+					<IonItem lines="none">
+						<IonLabel position="stacked">Repita sua nova senha</IonLabel>
+						<IonInput
+							id="confirmPassword"
+							name="confirmPassword"
+							placeholder="*********"
+							type={showRepeatNewPassword ? 'text' : 'password'}
+							{...register('confirmPassword')}
+						/>
+						<ErrorMessage
+							errors={errors}
+							name="confirmPassword"
+							as={<div style={{ color: 'red' }} />}
+						/>
+						<Button
+							className="absolute top-[35px] right-2 w-max z-[3] text-2xl bg-transparent"
+							type="button"
+							onClick={() => {
+								showRepeatNewPassword
+									? setShowRepeatNewPassword(false)
+									: setShowRepeatNewPassword(true)
+							}}
+						>
+							<IonIcon
+								icon={
+									showRepeatNewPassword ? eyeOffOutline : eyeOutline
+								}
+								className="text-[#AC8FBF]"
+							/>
+						</Button>
+					</IonItem>
+
+					<Button className="bg-purple-100 py-5 my-6 mt-16">
+						<IonText className="text-white text-lg">Redefinir</IonText>
+					</Button>
+					<Button className="bg-danger py-5 my-6">
+						<IonText className="text-white text-lg">Cancelar</IonText>
+					</Button>
+				</form>
 			</IonContent>
 		</IonPage>
 	)
